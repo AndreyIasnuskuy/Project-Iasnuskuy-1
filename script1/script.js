1c2e3d22685856af7b8581c4ddf7d137260837fb
Andrey Iasnuskuy <yasnuskuya@gmail.com>
2022-01-21 23:04:21 +0200

    Lesons 14/while,do,for

--- script1/script.js
+++ script1/script.js
@@ -185,3 +185,32 @@ switch (num) {
         console.log('Не цього разу');
         break;    
 }*/
+// Урок 14. Цикли
+
+//let num = 50;
+
+/*while (num <=55) {
+    console.log(num);
+    num++;
+}*/
+
+/*do {
+    console.log(num);
+    num++; 
+}
+while (num <=55);*/
+
+/*for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;  
+}*/
+
+/*for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        //break;
+        continue;
+    }
+
+    console.log(i);
+}*/
+
