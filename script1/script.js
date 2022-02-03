064f7c0323341108884190214d20b0cf67ea86c9
Andrey Iasnuskuy <yasnuskuya@gmail.com>
2022-02-03 23:34:42 +0200

    Lessons 22

--- script1/script.js
+++ script1/script.js
@@ -550,4 +550,96 @@ const products = str.split(",");
 products.sort();//Метод сортування
 console.log(products.join('; '));*/
 
-//Урок 22
+//Урок 22 Передача даних за посиланням або за значенням
+
+/*let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);*/
+
+/*const obj =  {
+    a: 5,
+    b: 1
+};
+
+const copy = obj; //Посилання(cсилка)
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj);*/
+
+//Поверхнісна копія обєктів
+
+/*function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+}
+
+const numbers = {
+    a:2,
+    b:5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a =10;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+console.log(numbers);*/
+
+/*const add = {
+    d: 17,
+    e: 20
+};
+
+console.log(Object.assign(numbers, add));*/
+
+/*const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'adadsda';
+console.log(newArray);
+console.log(oldArray);*/
+ 
+/*const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'lifejournal', 'blogger'],
+      internet = [...video, ...blogs, 'facebook'];
+
+console.log(internet);*/   
+
+/*function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);*/
+
+
+/*const array = ["a", "b"];
+
+const newAarray = [...array];*/
+
+/*const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};*/
+
