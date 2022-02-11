7325b475be61f692dd323f37a1dc29c8c905b8b8
Andrey Iasnuskuy <yasnuskuya@gmail.com>
2022-02-11 10:40:32 +0200

    Lesons 26

--- script1/script.js
+++ script1/script.js
@@ -768,3 +768,57 @@ const personalMovieDB = {
     }
 };
 */
+
+/*//lesons26
+
+// To String
+
+//1)Рідко користуються
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+//2)Конкотинація
+
+console.log(typeof(5+ ''));
+
+const num = 5;
+
+console.log("http://vk.com/catalog/" + num);
+
+const fontSize = 26 + "px";
+
+//To Number
+//1)
+
+console.log(typeof(Number('4')));
+//2)Метод унарний плюс
+
+console.log(typeof(+ '5'));
+
+//3)
+console.log(typeof(parseInt("15px", 10)));//Не часто вик.
+// Унарний прлюс 3)
+let answ = +prompt("Hello", ""); 
+
+//To boolean(false,true)
+
+ //0, '', null, undefined, NaN;
+//1)
+
+ let switcher = null;
+
+ if (switcher) {
+     console.log('Working...');
+ }
+
+ switcher = 1;
+
+if (switcher) {
+     console.log('Working...');
+ }
+
+ //2)Не корист.
+ console.log(typeof(Boolean('4')));
+
+//3)
+console.log(typeof(!!"5555"));*/
